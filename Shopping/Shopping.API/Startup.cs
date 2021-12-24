d5b465cc42ff05610e628d85f9167936a3e665ef
Xu Xue <xxue60@gatech.edu>
2021-12-23 23:13:56 -0600

    commit update to date code

--- Shopping/Shopping.API/Startup.cs
+++ Shopping/Shopping.API/Startup.cs
@@ -1,14 +1,18 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Shopping.API.Data;
 
-namespace Shopping.Client
+namespace Shopping.API
 {
     public class Startup
     {
@@ -22,12 +26,14 @@ namespace Shopping.Client
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddHttpClient("ShoppingAPIClient", client =>
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
             {
-                //client.BaseAddress = new Uri("http://localhost:5000/"); // Shopping.API url     
-                client.BaseAddress = new Uri(Configuration["ShoppingAPIUrl"]);
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Shopping.API", Version = "v1" });
             });
-            services.AddControllersWithViews();
+
+            services.AddScoped<ProductContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,12 +42,9 @@ namespace Shopping.Client
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Shopping.API v1"));
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -49,9 +52,7 @@ namespace Shopping.Client
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllers();
             });
         }
     }
--- Shopping/Shopping.Client/Controllers/HomeController.cs
+++ Shopping/Shopping.Client/Controllers/HomeController.cs
@@ -6,22 +6,29 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Shopping.Client.Data;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace Shopping.Client.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly HttpClient _httpClient;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IHttpClientFactory httpClientFactory, ILogger<HomeController> logger)
         {
-            _logger = logger;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _httpClient = httpClientFactory.CreateClient("ShoppingAPIClient");
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View(ProductContext.Products);
+            var response = await _httpClient.GetAsync("/product");
+            var content = await response.Content.ReadAsStringAsync();
+            var productList = JsonConvert.DeserializeObject<IEnumerable<Product>>(content);
+
+            return View(productList);
         }
 
         public IActionResult Privacy()
